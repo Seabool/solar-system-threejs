766c5258500c56768e706474381766b97816b430
Sebastian Stachecki <xenophobiouspl@gmail.com>
2021-06-01 11:09:56 +0200

    refactor

--- js/main.js
+++ js/main.js
@@ -18,7 +18,9 @@ renderer.setClearColor("#000000");
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Loading textures
 const textureEarth = new THREE.TextureLoader().load("img/earth.jpg");
+const textureMoon = new THREE.TextureLoader().load("img/moon.jpg");
 const textureSun = new THREE.TextureLoader().load("img/sun.jpg");
 const textureMercury = new THREE.TextureLoader().load("img/mercury.jpg");
 const textureVenus = new THREE.TextureLoader().load("img/venus.jpg");
@@ -44,61 +46,63 @@ function rotatePlanet(planet, self, speed, distance) {
     planet.position.z = Math.sin(time * (speed / (10 + 10))) * distance;
 }
 
-var sphereSun = createPlanet(10, textureSun);
-var sphereMercury = createPlanet(0.5, textureMercury);
-var sphereVenus = createPlanet(1, textureVenus);
-var sphereEarth = createPlanet(1, textureEarth);
-var sphereMars = createPlanet(0.6, textureMars);
-var sphereJupiter = createPlanet(4, textureJupiter);
-var sphereSaturn = createPlanet(3, textureSaturn);
-var sphereUranus = createPlanet(0.8, textureUranus);
-var sphereNeptune = createPlanet(1.5, textureNeptune);
+var sphereSun = createPlanet(5, textureSun);
+var sphereMercury = createPlanet(0.25, textureMercury);
+var sphereVenus = createPlanet(0.5, textureVenus);
+var sphereEarth = createPlanet(0.5, textureEarth);
+var sphereMoon = createPlanet(0.1, textureMoon);
+var sphereMars = createPlanet(0.3, textureMars);
+var sphereJupiter = createPlanet(2, textureJupiter);
+var sphereSaturn = createPlanet(1.5, textureSaturn);
+var sphereUranus = createPlanet(0.4, textureUranus);
+var sphereNeptune = createPlanet(0.75, textureNeptune);
 
 scene.add(sphereSun);
 scene.add(sphereMercury);
 scene.add(sphereVenus);
-scene.add(sphereEarth);
+
+const groupEarth = new THREE.Group();
+sphereMoon.position.x += 3;
+groupEarth.add(sphereEarth);
+groupEarth.add(sphereMoon);
+scene.add(groupEarth);
+
 scene.add(sphereMars);
 scene.add(sphereJupiter);
 
-const geometry = new THREE.RingGeometry( 4, 5, 32 );
-const material = new THREE.MeshBasicMaterial( { map: textureSaturn, side: THREE.DoubleSide } );
-const mesh = new THREE.Mesh( geometry, material );
-mesh.rotation.x -= 1.6;
+const geometrySaturnRing = new THREE.RingGeometry(4, 5, 32);
+const materialSaturnRing = new THREE.MeshBasicMaterial({
+    map: textureSaturn,
+    side: THREE.DoubleSide,
+});
+const meshSaturnRing = new THREE.Mesh(geometrySaturnRing, materialSaturnRing);
+meshSaturnRing.rotation.x -= 1.6;
 
-const group = new THREE.Group();
-group.add( sphereSaturn );
-group.add( mesh );
-
-scene.add( group );
-
-
-//scene.add(sphereSaturn);
-
+const groupSaturn = new THREE.Group();
+groupSaturn.add(sphereSaturn);
+groupSaturn.add(meshSaturnRing);
+scene.add(groupSaturn);
 
 scene.add(sphereUranus);
 scene.add(sphereNeptune);
-
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
 var render = function () {
     requestAnimationFrame(render);
-
     var distanceFromSun = 150;
 
     rotatePlanet(sphereSun, 0.02, 0, 0);
     rotatePlanet(sphereMercury, 0.0001, 4.7, distanceFromSun * 0.1);
     rotatePlanet(sphereVenus, 0.000065, 3.5, distanceFromSun * 0.2);
-    rotatePlanet(sphereEarth, 0.0157, 2.98, distanceFromSun * 0.3);
+    rotatePlanet(groupEarth, 0.2, 2.98, distanceFromSun * 0.3);
     rotatePlanet(sphereMars, 0.008, 2.41, distanceFromSun * 0.4);
     rotatePlanet(sphereJupiter, 4.58, 1.31, distanceFromSun * 0.5);
-    rotatePlanet(group, 3.68, 0.97, distanceFromSun * 0.6);
+    rotatePlanet(groupSaturn, 3.68, 0.97, distanceFromSun * 0.6);
     rotatePlanet(sphereUranus, 1.4, 0.68, distanceFromSun * 0.7);
     rotatePlanet(sphereNeptune, 0.09, 0.54, distanceFromSun * 0.8);
 
     controls.update();
-
     renderer.render(scene, camera);
 };
 
